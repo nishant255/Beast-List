3d4b52068747f1fd74389a2c7ab1ce9ccc36321c
Nishant Patel <contact@nishant-patel.com>
2017-03-14 13:40:29 -0700

    Final Commit

--- Beast List/ViewController.swift
+++ Beast List/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var tasks = ["Todo 1","Finish MidTerm"]
 
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        tasks.append(taskTextField.text!)
+        taskTextField.text = nil
+        tableView.reloadData()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +44,16 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
